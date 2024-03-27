dd13c45d0871ff99797f56c0281227b61208c87b
engageintellect <jc9361@gmail.com>
2024-03-26 20:58:13 -0700

    init

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from mnemonic import Mnemonic
+from bitcoin import *
 from hdwallet import BIP32HDWallet
 import uvicorn
 
@@ -10,16 +11,27 @@ app = FastAPI()
 
 def generate_seed_phrase():
     mnemonic = Mnemonic("english")
-    return mnemonic.generate(256)  # 256 bits is a standard BIP39 seed length
+    # Generate a 128 bits BIP39 seed for a 12 word mnemonic
+    return mnemonic.generate(128)  # Use 128 bits for 12-word seed phrase
 
 # Generate a Bitcoin address and private key from a seed phrase
 
 
 def generate_bitcoin_keys(seed_phrase):
     wallet = BIP32HDWallet()  # Initialize the wallet
-    wallet.from_mnemonic(mnemonic=seed_phrase)  # Set the mnemonic
+    wallet.from_mnemonic(mnemonic=seed_phrase, language="english")
+
+    # Specify the derivation path for the first address
+    wallet.from_path("m/44'/0'/0'/0/0")
+
     private_key = wallet.private_key()
-    address = wallet.address()
+    # Ensure you're using the compressed public key
+    public_key = wallet.public_key(compressed=True)
+
+    # Generate P2PKH address from the compressed public key
+    # 0 is the magic byte for mainnet
+    address = pubtoaddr(public_key, magicbyte=0)
+
     return private_key, address
 
 
@@ -31,10 +43,14 @@ def generate_wallet():
     # Generate a Bitcoin private key and address from the seed phrase
     private_key, bitcoin_address = generate_bitcoin_keys(seed_phrase)
 
-    # Return the seed phrase, Bitcoin private key, and Bitcoin address
+    # Convert the private key to WIF format (compressed)
+    private_key_wif = encode_privkey(
+        decode_privkey(private_key, 'hex'), 'wif_compressed')
+
+    # Return the seed phrase, Bitcoin WIF private key, and Bitcoin address
     payload = {
         "btcAddress": bitcoin_address,
-        "privateKey": private_key,
+        "privateKey": private_key_wif,
         "seedPhrase": seed_phrase
     }
 
